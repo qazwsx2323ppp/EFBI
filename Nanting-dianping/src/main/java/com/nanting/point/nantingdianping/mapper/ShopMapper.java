29d2ba5d7e1687cde2313036e289d7ba1fe8349c
zbj <3168078770@qq.com>
2025-04-28 18:54:36 +0800

    feat: 定义ShopType，增加dao层

---
+++ Nanting-dianping/src/main/java/com/nanting/point/nantingdianping/mapper/ShopMapper.java
@@ -0,0 +1,12 @@
+package com.nanting.point.nantingdianping.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.nanting.point.nantingdianping.entity.Shop;
+
+/**
+ * @Description:
+ * @Author: zbj
+ * @Date: 2025/4/28
+ */
+public interface ShopMapper extends BaseMapper<Shop> {
+}
